5f8479976953e59329fc193324e177e8a59736e1
kakashi <petunilo96@gmail.com>
2025-06-21 18:58:37 -0300

    remove granade class and sprites

--- Entities/main_character.py
+++ Entities/main_character.py
@@ -29,11 +29,6 @@ class MainCharacter(Character):
         self.crounch_frame = pygame.image.load(
             "Sprites/main_character/crouch_0.png"
         ).convert_alpha()  # frame de agachado
-        self.shoot_granade_frames = []  # Sprite de disparo con granada
-        self.load_shoot_granade_sprites(
-            "Sprites/main_character/granade", frame_count=7
-        )
-        self.is_granade = False # para que el personaje dispare granadas
         self._is_stuck = False
 
     def load_walk_sprites(self, folder_path, frame_count):
@@ -63,14 +58,6 @@ class MainCharacter(Character):
             frame = pygame.transform.scale(frame, (60, 80))
             frames.append(frame)
         return frames
-
-    def load_shoot_granade_sprites(self, folder_path, frame_count):
-        # recorro el numero de frames de la animacion de disparo con granada
-        for i in range(frame_count):
-            path = f"{folder_path}/{i}.png"
-            frame = pygame.image.load(path).convert_alpha()
-            frame = pygame.transform.scale(frame, (60, 80))
-            self.shoot_granade_frames.append(frame)
 
     def draw(self, surface, scroll_x=0):
         # Actualiza la animación si se está moviendo
@@ -102,36 +89,18 @@ class MainCharacter(Character):
             frame = pygame.transform.scale(self.crounch_frame, (70, 80)) # escalamos el frame de agachado
             surface.blit(frame, (self.x - scroll_x, self.y - self.height)) # restamos para igualar la altura del personaje con el sprite
             # personaje disparando una granada
-        elif self.is_granade:
-            self.frame_timer += 1
-            if self.frame_timer >= self.frame_rate:
-                self.current_frame = (self.current_frame + 1) % len(self.shoot_granade_frames)
-            frame = self.shoot_granade_frames[self.current_frame]
-            surface.blit(frame, (self.x - scroll_x, self.y - self.height))
         else:
             # personaje quieto: mostramos el primer frame sin animar
             self.current_frame = 0
             self.frame_timer = 0
             frame = self.walk_frames[0]
             surface.blit(frame, (self.x - scroll_x, self.y - self.height))  # dibujamos el personaje en la pantalla
-        #  # Fuente para dibujar texto
-        # font = pygame.font.SysFont(None, 24)
-        
-        # # Crear texto con la posición
-        # coord_text = font.render(f"({self.x}, {self.y}, {self.height})", True, (0, 0, 0))  # negro
-        
-        # # Dibujar texto sobre el personaje
-        # surface.blit(coord_text, (self.x + 50, self.y - 20))  # un poco más arriba
 
     # TODO: por que "camina" el personaje cuando no lo estoy moviendo?
     def move(self, keys):
         scroll_amount = 0
-
         if not keys[pygame.K_DOWN]:
             self.stand()
-        if not keys[pygame.K_g]:
-        # TODO: terminar de configurar la granada
-            self.is_granade = False
         if keys[pygame.K_LEFT]:
             if self.x > 0:
                 self.x -= self.speed
@@ -146,8 +115,6 @@ class MainCharacter(Character):
             self.crouch()
         if keys[pygame.K_UP]:
             self.jump()
-        if keys[pygame.K_g] and not self.is_granade:
-            self.is_granade = True
         self.apply_gravity()
         if self.is_jumping or self.y < self.initial_y:
             self.is_stuck = False
