d0b9a48c86d753d0fdb7576b3c3a2661bf835c56
kakashi <petunilo96@gmail.com>
2025-06-21 17:56:19 -0300

    update

--- Entities/main_character.py
+++ Entities/main_character.py
@@ -72,8 +72,9 @@ class MainCharacter(Character):
             if self.frame_timer >= self.frame_rate:
                 self.current_frame = (self.current_frame + 1) % len(self.shoot_frames) # cambiamos el frame cuando se cumple el frame_rate
                 self.frame_timer = 0
+            if self.current_frame >= len(self.shoot_frames):
+                self.current_frame = 0
             # dibujamos el frame de disparo
-            # TODO: esta linea genera un list index out of range
             frame = self.shoot_frames[self.current_frame] 
             surface.blit(frame, (self.x - scroll_x, self.y - self.height)) # restamos para igualar la altura del personaje con el sprite
         elif keys[pygame.K_LEFT] or keys[pygame.K_RIGHT]:
